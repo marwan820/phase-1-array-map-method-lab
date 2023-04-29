e6a123eeee05d8fa3c7aa2b889a6401b7ba47cc0
Marwan Rahim <marwan820@Marwan>
2023-04-29 13:51:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (array) => {
+  return tutorials.map((tutorial) => {
+    return tutorial.split(" ").map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ")})
 }
